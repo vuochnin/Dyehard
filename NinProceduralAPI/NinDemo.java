880601ee057f57fd11d43db9aabfd301a560bd62
Vuochly Ky <ninvk@uw.edu>
2016-06-25 16:13:10 -0700

    Added a new project NinProceduralAPI

---
+++ NinProceduralAPI/NinDemo.java
@@ -0,0 +1,12 @@
+import NinDHProcedrualAPI.NinProcedrualAPI;
+
+/**
+ * NOTE: NO NEED TO PUT ANY CODE IN THIS CLASS YET
+ * (THE GAME WILL RUN FROM NinProceduralAPI.java)
+ * 
+ * @author vuochnin
+ *
+ */
+public class NinDemo extends NinProcedrualAPI{
+	
+}
