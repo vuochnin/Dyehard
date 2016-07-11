b8794c15b7399ecadb738efe0113f1598619f842
Vuochly Ky <ninvk@uw.edu>
2016-07-11 10:31:37 -0700

    EnemyGenerator updates

--- DHProceduralAPI/dyeHardProcedrualAPI/EnemyGenerator.java
+++ DHProceduralAPI/dyeHardProcedrualAPI/EnemyGenerator.java
@@ -55,13 +55,21 @@ public class EnemyGenerator {
     }
     
     public void spawnEnemy(){
-		switch (RANDOM.nextInt(3)) {
+		switch (RANDOM.nextInt(5)) {
         case 1:
             //EnemyManager.getInstance().registerEnemy(new PortalEnemy(randomPosition(), hero));
         	count++;
             break;
         case 2:
         	EnemyManager.getInstance().registerEnemy(new ChargerEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        case 3:
+        	EnemyManager.getInstance().registerEnemy(new ShootingEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        case 4:
+        	EnemyManager.getInstance().registerEnemy(new CollectorEnemy(randomPosition(), hero));
         	count++;
             break;
         default:
@@ -71,15 +79,52 @@ public class EnemyGenerator {
         }
     }
     
+    public void spawnEnemy(String EnemyType){
+    	String type = EnemyType.toLowerCase();
+    	switch(type){
+    	case "portal":
+    		//EnemyManager.getInstance().registerEnemy(new PortalEnemy(randomPosition(), hero));
+        	//count++;
+            break;
+    	case "charger":
+        	EnemyManager.getInstance().registerEnemy(new ChargerEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        case "shooting":
+        	EnemyManager.getInstance().registerEnemy(new ShootingEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        case "collector":
+        	EnemyManager.getInstance().registerEnemy(new CollectorEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        case "regular":
+        	EnemyManager.getInstance().registerEnemy(new RegularEnemy(randomPosition(), hero));
+        	count++;
+            break;
+        default:
+        	System.out.println("ERROR: The type of enemy you entered is not existed");
+        	break;
+    	}
+    }
+    
     public void spawnEnemy(float height){
         Vector2 position = new Vector2(BaseCode.world.getWidth(), height);
-		switch (RANDOM.nextInt(3)) {
+		switch (RANDOM.nextInt(5)) {
         case 1:
             //EnemyManager.getInstance().registerEnemy(new PortalEnemy(position, hero));
         	count++;
             break;
         case 2:
         	EnemyManager.getInstance().registerEnemy(new ChargerEnemy(position, hero));
+        	count++;
+            break;
+        case 3:
+        	EnemyManager.getInstance().registerEnemy(new ShootingEnemy(position, hero));
+        	count++;
+            break;
+        case 4:
+        	EnemyManager.getInstance().registerEnemy(new CollectorEnemy(position, hero));
         	count++;
             break;
         default:
