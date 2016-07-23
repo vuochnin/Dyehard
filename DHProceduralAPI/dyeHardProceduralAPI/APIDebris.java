1aec7e3bd1271d5f46a308a5150703c1aebb443b
Vuochly Ky <ninvk@uw.edu>
2016-07-23 12:57:24 -0700

    APIDebris class, for the types of debris

---
+++ DHProceduralAPI/dyeHardProceduralAPI/APIDebris.java
@@ -0,0 +1,36 @@
+package dyeHardProceduralAPI;
+
+import java.util.Random;
+
+import Engine.BaseCode;
+import dyehard.Obstacles.Debris;
+
+public class APIDebris extends Debris {
+	int type;
+	public static String typeName;
+	private static Random RANDOM = new Random();
+	
+	public APIDebris(int type, float minX, float maxX){
+		super(minX, maxX);
+		this.type = type;
+		switch (RANDOM.nextInt(3)) {
+        case 0:
+            texture = BaseCode.resources.loadImage("Textures/Debris/debris_01.png");
+            typeName = "Plate";
+            break;
+        case 1:
+            texture = BaseCode.resources.loadImage("Textures/Debris/debris_02.png");
+            typeName = "Light";
+            break;
+        case 2:
+            texture = BaseCode.resources.loadImage("Textures/Debris/debris_03.png");
+            typeName = "Pipe";
+            size.setX(size.getY() * 1.28f);
+            break;
+        }
+	}
+	
+	public String toString(){
+		return typeName;
+	}
+}
